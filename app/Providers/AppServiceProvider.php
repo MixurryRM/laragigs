fac5de3c352b8a455a6d32782a4b9c3e951d9f3b
Mixurry RM <mixurrymr@gmail.com>
2023-12-18 22:35:41 +0630

    laragigs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Pagination\Paginator;
+// use Illuminate\Pagination\Paginator;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,6 +25,5 @@ class AppServiceProvider extends ServiceProvider
 
         // Paginator::useBootstrapFive();
     }
-
 
 }
